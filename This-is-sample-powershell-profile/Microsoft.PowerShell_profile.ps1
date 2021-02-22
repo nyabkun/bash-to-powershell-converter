2188fd6b319089b419df8a94f84aebabf2690bec
nyabkun <nyabkun@gmail.com>
2021-02-22 21:26:19 +0900

    remove comments

--- This-is-sample-powershell-profile/Microsoft.PowerShell_profile.ps1
+++ This-is-sample-powershell-profile/Microsoft.PowerShell_profile.ps1
@@ -5,9 +5,6 @@
 node $PSScriptRoot/bash2pwsh.js C:/msys64/home/owner/.alias.zsh ./bash_alias.ps1
 
 # https://stackoverflow.com/a/47075453/14820021
-# & ./bash_alias.ps1
-# Invoke-Expression "./bash_alias.ps1"
 ."./bash_alias.ps1"
 
-# PS1='\[`[ $? = 0 ] && X=2 || X=1; tput setaf $X`\]\h\[`tput sgr0`\]:$PWD\n\$ '
 echo "Loaded : $PSCommandPath"
