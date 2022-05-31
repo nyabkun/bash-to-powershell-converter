245d9228e47aae93be6517b080ca57f87e7c0a35
nyabkun <nyabkun@gmail.com>
2022-06-01 06:29:14 +0900

    Modify comment

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,46 +1,45 @@
-
-# https://stackoverflow.com/q/9406434/14820021
-# ."./wrap.ps1"
-# ."./color_ls.ps1"
-
-# C:/Users/owner/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
-#
-# Hint ::
-# New-Item -ItemType HardLink -Name .alias.zsh -Value C:/msys64/home/owner/.alias.zsh
-#
-
 $BashAliasPwsh = "$PSScriptRoot/bash_alias.ps1"
 $Bash2Pwsh = "$PSScriptRoot/bash2pwsh.js"
 $BashAlias = "$PSScriptRoot/.alias.zsh"
 
+function bash2pwsh_alias() {
+	node $Bash2Pwsh $BashAlias $BashAliasPwsh
+	echo "node $Bash2Pwsh $BashAlias $BashAliasPwsh"
+}
+
 $PWSHProfile = $PSCommandPath
 
-# Hard link
-function ln($src, $dest) {
-	New-Item -ItemType HardLink -Name "$dest" -Value "$src"
-}
-
-function l() {
-	ls | Sort-Object Mode, Name, LastWriteTime
-	# dir | Sort-Object LastWriteTime
-}
-
-function ls_last_write_time() {
-	ls | Sort-Object Mode, LastWriteTime
-	# dir | Sort-Object LastWriteTime
-}
-
-function touch {
-	echo $null >> $args
-}
+# if you want to use bash like command in powershell,
+# it is recommended to install gow.
+# "scoop install gow"
 
 function edit_pwsh_profile {
 	code $PWSH_Profile
+	echo "code $PWSHProfile"
 }
+
+# https://stackoverflow.com/a/69135912/5570400
+#
+# Pipe output to vscode
+#
+# Usage :
+#   ls | oc
+Function Out-Code {
+    do {
+        $filename = New-Guid
+		$filename = $filename.toString() + ".md"
+    } while (Test-Path $filename)
+    $input > $filename
+    code $filename
+    Start-Sleep 1
+    Remove-Item $filename
+}
+Set-Alias oc Out-Code
 
 # reload profile script
 function re {
 	. $PWSHProfile
+	echo ". $PWSHProfile"
 }
 
 # https://stackoverflow.com/a/47075453/14820021
